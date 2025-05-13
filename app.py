bcbc3f68caf885059c4f53f351dbb688685f25a7
Gustavo Jannuzzi <80784697+GustavoJannuzzi@users.noreply.github.com>
2025-05-13 19:34:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,11 @@ def display_exif_data(image):
 
 # Função para converter coordenadas GPS de DMS para formato decimal
 def convert_to_decimal(degrees, minutes, seconds):
-    return degrees + (minutes / 60.0) + (seconds / 3600.0)
+    try:
+        return degrees + (minutes / 60.0) + (seconds / 3600.0)
+    except Exception as e:
+        st.error(f"Erro ao converter coordenadas GPS: {e}")
+        return None
 
 # Função para extrair os metadados GPS
 def get_location_from_exif(exif_data):
@@ -37,17 +41,18 @@ def get_location_from_exif(exif_data):
 
             if gps_info is not None:
                 # Extraímos a latitude e longitude
-                lat_deg = gps_info[2][0] / gps_info[2][1]
-                lat_min = gps_info[2][1] / gps_info[2][2]
-                lat_sec = gps_info[2][2] / gps_info[2][3]
-                lat = convert_to_decimal(lat_deg, lat_min, lat_sec)
+                lat_deg = gps_info[2][0]
+                lat_min = gps_info[2][1]
+                lat_sec = gps_info[2][2]
+                lon_deg = gps_info[4][0]
+                lon_min = gps_info[4][1]
+                lon_sec = gps_info[4][2]
 
-                lon_deg = gps_info[4][0] / gps_info[4][1]
-                lon_min = gps_info[4][1] / gps_info[4][2]
-                lon_sec = gps_info[4][2] / gps_info[4][3]
+                # Convertendo as coordenadas para o formato decimal
+                lat = convert_to_decimal(lat_deg, lat_min, lat_sec)
                 lon = convert_to_decimal(lon_deg, lon_min, lon_sec)
 
-                # Exibir as coordenadas GPS
+                # Retorna as coordenadas GPS
                 return lat, lon
         return None, None
     except Exception as e:
